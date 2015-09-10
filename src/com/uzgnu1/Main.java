91a7a09582109955ea2a8b18ca5c207545ac3795
ivan9999 <ximinez16@mail.ru>
2015-09-10 11:21:57 +0400

    додав

--- src/com/uzgnu1/Main.java
+++ src/com/uzgnu1/Main.java
@@ -3,6 +3,11 @@ package com.uzgnu1;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
     }
+
+    System.out.print(50 + 20);
+
+
+
 }
